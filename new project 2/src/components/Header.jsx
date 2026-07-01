c1a655b019bfc35b799456b23c6d1ca509550abb
Ryan Hyson Dsilva <ryandsilva34@gmail.com>
2026-07-01 21:36:28 +0530

    new

--- new project 2/src/components/Header.jsx
+++ new project 2/src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 import AuthModal from "./AuthModal";
 
 function Header() {
--- new project 2/src/main.jsx
+++ new project 2/src/main.jsx
@@ -3,7 +3,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './components/AuthContext'
+import { AuthProvider } from './Context/AuthContext'
 import './index.css'
 import App from './App.jsx'
 
