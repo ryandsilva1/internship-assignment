febeeff8127481148a72fc1b850c1f3ae0d2d53a
Ryan Hyson Dsilva <ryandsilva34@gmail.com>
2026-07-01 21:34:26 +0530

    Update main.jsx

--- new project 2/src/main.jsx
+++ new project 2/src/main.jsx
@@ -3,7 +3,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './components/AuthContext'
 import './index.css'
 import App from './App.jsx'
 
