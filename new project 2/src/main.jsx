32d0afaaad7dfcb2a5924076288b431b293d2050
Ryan Hyson Dsilva <ryandsilva34@gmail.com>
2026-07-02 10:47:19 +0530

    t

--- new project 2/src/main.jsx
+++ new project 2/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
