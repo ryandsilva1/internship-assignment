450678c234361dc71941fc52849e293d62ebc4d8
Ryan Hyson Dsilva <ryandsilva34@gmail.com>
2026-07-02 10:44:07 +0530

    Update main.jsx

--- new project 2/src/main.jsx
+++ new project 2/src/main.jsx
@@ -1,10 +1,8 @@
-// src/main.jsx
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { CartProvider } from './context/CartContext.js'
+import { CartProvider } from './context/CartContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
